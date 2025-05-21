7fb3587c688daaaf99fe0c8efa494cc40954817c
fachry2005 <frssfachry@gmail.com>
2025-05-21 19:39:14 +0700

    PR Pertemuan 11

---
+++ resources/views/frontend.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Halaman Pertemuan</title>
+</head>
+<body>
+    <h1>Pemrograman WEB</h1>
+    <ul>
+         <li><a href="{{ url('/Index ETS') }}">Index ETS</a></li>
+         <li><a href="{{ url('Index') }}">Index</a></li>
+         <li><a href="{{ url('/JS1') }}">JS1</a></li>
+         <li><a href="{{ url('/pertama') }}">pertama</a></li>
+         <li><a href="{{ url('/Pertemuan 4') }}">Pertemuan 4</a></li>
+         <li><a href="{{ url('/Template1') }}">Template 1</a></li>
+         <li><a href="{{ url('/Tugas Linktree') }}">Tugas Linktree</a></li>
+         <li><a href="{{ url('/Validasi1') }}">Validasi1</a></li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,62 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//
+use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend');
 });
 
 
-Route::get('halo', function () {
-	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
+Route::get('/blog', function () {
+    return view('blog');
 });
 
-Route::get('blog', function () {
-	return view('blog');
+Route::get('/Index ETS', function () {
+    return view('Index ETS');
 });
 
-Route::get('satu', function () {
-	return view('pertama');
+Route::get('/Index', function () {
+    return view('Index');
 });
+
+Route::get('/JS1', function () {
+    return view('JS1');
+});
+
+Route::get('/pertama', function () {
+    return view('pertama');
+});
+
+Route::get('/Pertemuan 4', function () {
+    return view('Pertemuan 4');
+});
+
+Route::get('/Template1', function () {
+    return view('Template1');
+});
+
+Route::get('/Tugas Linktree', function () {
+    return view('Tugas Linktree');
+});
+
+Route::get('/Validasi1', function () {
+    return view('Validasi1');
+});
+
+
+
