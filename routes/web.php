6dfce81307accbbcb5750c371542393f0df0cd9e
fachry2005 <frssfachry@gmail.com>
2025-05-22 15:55:13 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Link;
+use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\BlogController;
 
@@ -59,4 +59,13 @@ Route::get('/Validasi1', function () {
 });
 
 
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('welcome', [DosenController::class, 'welcome']);
 
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
